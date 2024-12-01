2387ffa84a10f5ea63ef370ea2f9bb3f6c4f3351
Juan Cruz <juan.garcia01@davinci.edu.ar>
2024-12-01 19:43:50 -0300

    control de ingreso a panel admin y ruta singup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link, Navigate  } from 'react-router-dom';
-
-// Importa las páginas
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
@@ -11,9 +9,13 @@ import Login from './pages/Login';
 import Cart from './pages/Cart';
 import AdminPanel from './components/AdminPanel';
 import AgregarProducto from './components/AgregarProducto';
-import AgregarUsuario from './components/AgregarUsuario';
 import ProductosTable from './components/ProductosTabla';
-import ModificarProducto from './components/ModificarProduto';
+import ModificarProducto from './components/ModificarProducto';
+import ModificarUsuario from './components/ModificarUsuario';
+import UsuariosTabla from './components/UsuariosTabla';
+import Signup from './pages/Signup';
+import RutasProtejidas from './components/RutasProtejidas'; // Importa el componente de ruta protegida
+import AgregarUsuarioAdmin from './components/agregarUsuarioAdmin';
 
 function App() {
   return (
@@ -25,12 +27,15 @@ function App() {
         <Route path="/gallery" element={<Gallery />} />
         <Route path="/product/:id" element={<Product />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/admin" element={<AdminPanel/>} />
-        <Route path="/productostabla" element={<ProductosTable/>} />
-        <Route path="/agregar-producto" element={<AgregarProducto />} />
-        <Route path="/agregar-usuario" element={<AgregarUsuario />} />
-        <Route path="/modificar-producto/:id" element={<ModificarProducto />} />
-        <Route path="/cart" element={<Cart/>} />
+        <Route path="/admin" element={<RutasProtejidas element={<AdminPanel />} />} />
+        <Route path="/productostabla" element={<RutasProtejidas element={<ProductosTable />} />} />
+        <Route path="/agregar-producto" element={<RutasProtejidas element={<AgregarProducto />} />} />
+        <Route path="/agregar-usuario" element={<RutasProtejidas element={<AgregarUsuarioAdmin />} />} />
+        <Route path="/modificar-producto/:id" element={<RutasProtejidas element={<ModificarProducto />} />} />
+        <Route path="/modificar-usuario/:id" element={<RutasProtejidas element={<ModificarUsuario />} />} />
+        <Route path="/usuariostabla" element={<RutasProtejidas element={<UsuariosTabla />} />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </Router>
   );
